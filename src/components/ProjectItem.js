296d11d83e7428e7a19e4a36996f46df8c623aa0
Yeji-Yoon <angelayoon0102@gmail.com>
2025-02-19 18:51:15 -0800

    s

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
+//  <ProjectItem name={project.name} about={project.about} technologies={project.technologies}/>
+
+
 function ProjectItem({ name, about, technologies }) {
+  const listoftech= technologies.map((technology,index)=>{
+    return <span key={index}>{technology}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {listoftech}
       </div>
     </div>
   );
